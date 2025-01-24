659bfe6ada23d9653ce73734e42494d10f7e7628
AlejandraArevalo <ale22arevalo@gmail.com>
2025-01-24 10:29:48 -0800

    added ids

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,12 +24,7 @@ const bubbleMetric = 'Top 100 artists by Spotify Monthly Listeners';
 
   return (
     <main>
-      <div className="hero">
-        <Heading level={1} text={"HMC Chart Templates"} />
-      </div>
-      <div className="body-wrapper">
-        <Heading level={2} text={"A library of interactive charts following the aesthetic of HMC"} />
-        <div style={{backgroundColor: '#3F8FC2', padding: '1rem', borderRadius: '10px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)'}}>
+      <div id="viz_1">
           <LineChart 
             data={data} 
             title={title} 
@@ -43,16 +38,16 @@ const bubbleMetric = 'Top 100 artists by Spotify Monthly Listeners';
             image={badbunnyphoto}
           />
         </div>
-        <BubbleChart 
-          data={data} 
-          title={bubbleTitle} 
-          metric={bubbleMetric}
-          date = {date}
-          note = {note}
-          lineColor={"steelblue"}
-        />
-      </div>
-
+        <div id="viz_2">
+          <BubbleChart 
+            data={data} 
+            title={bubbleTitle} 
+            metric={bubbleMetric}
+            date = {date}
+            note = {note}
+            lineColor={"steelblue"}
+          />
+        </div>
     </main>
   );
 };
